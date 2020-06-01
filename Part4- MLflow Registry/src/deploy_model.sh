fe0b95ae33387b52e4b946b9edb48d7041fe2da8
Isaac Reis <isaacisforreal@gmail.com>
2020-06-01 12:25:46 +0100

    forlder restructure

--- Part4- MLflow Registry/src/deploy_model.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-echo "Deploying Production model name=MNISTmodel"
-
-# Set enviorment variable for the tracking URL where the Model Registry is
-export MLFLOW_TRACKING_URI=sqlite:///mlruns.db
-# Serve the production model from the model registry
-mlflow models serve --model-uri models:/MNISTmodel/production --no-conda
